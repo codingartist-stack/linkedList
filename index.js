8bd8b48fa99d5aad6306040598fd84ad98c5eb76
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-08-14 12:00:44 -0700

    doing the linked list

---
+++ index.js
@@ -0,0 +1,30 @@
+function linkedList(theList) {
+  const head = theList[0];
+  const tail = theList.length - 1;
+  const size = theList.length;
+
+  return {
+    head,
+    tail,
+    size,
+    append(value) {
+      theList.push(value);
+    },
+    prepend(value) {
+      theList.unshift(value);
+    },
+    at(index) {
+      return theList[index];
+    },
+    pop() {
+      theList.pop();
+    },
+  };
+}
+
+function linkedNode(value, nextNode = null) {
+  return {
+    value,
+    nextNode,
+  };
+}
