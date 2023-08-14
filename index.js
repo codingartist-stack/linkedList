04da08b52d1d95f86abd5a010a93f03fa631c4ac
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-08-14 13:20:02 -0700

    wrote a find function, but I am not sure if it works

--- index.js
+++ index.js
@@ -3,22 +3,51 @@ function linkedList(theList) {
   const tail = theList[theList.length - 1];
   const size = theList.length;
 
+  function append(value) {
+    theList.push(value);
+  }
+
+  function prepend(value) {
+    theList.unshift(value);
+  }
+
+  function at(index) {
+    return theList[index];
+  }
+
+  function pop() {
+    theList.pop();
+  }
+
+  function contains(value) {
+    //is the list sorted?
+    //returns true if on the list otherwise return false
+    if (theList.find(value) == undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function find(value) {
+    //returns the index or null if not found
+  }
+
+  function toString() {
+    //turns your linked list into strings
+    //format ( value ) -> ( value ) -> ( value ) -> null
+  }
+
   return {
     head,
     tail,
     size,
-    append(value) {
-      theList.push(value);
-    },
-    prepend(value) {
-      theList.unshift(value);
-    },
-    at(index) {
-      return theList[index];
-    },
-    pop() {
-      theList.pop();
-    },
+    append,
+    prepend,
+    at,
+    pop,
+    contains,
+    find,
   };
 }
 
