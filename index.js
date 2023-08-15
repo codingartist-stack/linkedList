8ac7fda5e3bdfb60aae9839f09319ca06d97f1d7
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-08-15 08:46:01 -0700

    I think I wrote everything, but I commented out most of it so I can test it one at a time. now I need to figure out how to test it.

--- index.js
+++ index.js
@@ -5,49 +5,57 @@ function linkedList(theList) {
 
   function append(value) {
     theList.push(value);
+    return theList;
   }
 
-  function prepend(value) {
-    theList.unshift(value);
-  }
+  //   function prepend(value) {
+  //     theList.unshift(value);
+  //     return theList;
+  //   }
 
-  function at(index) {
-    return theList[index];
-  }
+  //   function at(index) {
+  //     return theList[index];
+  //   }
 
-  function pop() {
-    theList.pop();
-  }
+  //   function pop() {
+  //     theList.pop();
+  //     return theList;
+  //   }
 
-  function contains(value) {
-    //is the list sorted?
-    //returns true if on the list otherwise return false
-    if (theList.find(value) == undefined) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+  //   function contains(value) {
+  //     //is the list sorted?
+  //     //returns true if on the list otherwise return false
+  //     return theList.includes(value);
+  //   }
 
-  function find(value) {
-    //returns the index or null if not found
-  }
+  //   function find(value) {
+  //     //returns the index or null if not found
+  //     return indexOf(value);
+  //   }
 
-  function toString() {
-    //turns your linked list into strings
-    //format ( value ) -> ( value ) -> ( value ) -> null
-  }
+  //   function toString() {
+  //     //turns your linked list into strings
+  //     //format ( value ) -> ( value ) -> ( value ) -> null
+  //     let stringList = '';
+
+  //     for (let i = 0; i < theList.length; i++) {
+  //       stringList + '( ' + theList[i] + ' ) -> ';
+  //     }
+
+  //     return stringList + 'null';
+  //   }
 
   return {
     head,
     tail,
     size,
     append,
-    prepend,
-    at,
-    pop,
-    contains,
-    find,
+    // prepend,
+    // at,
+    // pop,
+    // contains,
+    // find,
+    // toString,
   };
 }
 
